6f2ee4c3c739f309810af228c295fa35880b17f7
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-05-20 20:51:04 +0900

    ex1 done

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,27 @@ export default function App() {
   ];
 
   // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
-  const filteredStudents = students.filter(/* 여기에 코드 작성 */);
+  const filteredStudents = students.filter((student) => student.age >= 18);
+
+  // TODO: 학생이름을 클릭하면 나이와 점수를 alert로 보여줌
+  const handleStudentClick = (student) => {
+    alert(`${student.age}세, ${student.grade}등급`);
+  };
 
   return (
     <div>
       <h1>학생 목록</h1>
       <ul>
         {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
-        {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
+        {filteredStudents.map((student) => {
+          return (
+            <li key={student.name}>
+              <button onClick={() => handleStudentClick(student)}>
+                {student.name}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
